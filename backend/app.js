7abd455df722872de09037e299858d475567223e
wndyd <wndyd@JUYONGui-MacBookAir.local>
2022-11-11 00:19:51 +0900

    feat: db(mongodb) connection added

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.port || 8000;
+const connectDB = require('./db/connect');
 
-app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)});
+require('dotenv').config();
+
+const run = async() => {
+    try{
+        await connectDB(process.env.MONGO_URI);
+        app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+    }
+    catch(err){
+        console.log(err);
+    }
+};
+
+run();
