49e79ecec207842aa4f309868a0f71b2c88a7631
vdnuit <97578112+vdnuit@users.noreply.github.com>
2022-11-10 18:37:45 +0900

    Settings

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+    return (
+        <>
+            <p>congraduation</p>
+            <p>congraduation</p>
+        </>
+    );
+}
+
+export default App;
