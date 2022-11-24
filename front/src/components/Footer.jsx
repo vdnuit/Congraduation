8105b6b3884638a7288063f90aef68acbdbd724c
vdnuit <v_nuit@naver.com>
2022-11-24 21:11:11 +0900

    ModalCheck

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -1,13 +1,22 @@
 import styled from 'styled-components';
-import React from 'react';
+import React, { useState } from 'react';
+import ModalCheck from './ModalCheck';
 
 const Container = styled.div``;
 const Text = styled.p``;
 
 function Footer() {
+    const [checkOpen, setCheckOpen] = useState(false);
+    const showCheck = () => {
+        setCheckOpen(true);
+    };
     return (
         <Container>
             <Text>Footer</Text>
+            <button type="button" onClick={showCheck}>
+                checkChecktest
+            </button>
+            {checkOpen && <ModalCheck setCheckOpen={setCheckOpen} text="모달창 체크입니다." />}{' '}
         </Container>
     );
 }
