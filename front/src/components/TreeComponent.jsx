ce903aed1c21a54fc23b10e43217a6d24bcee568
amoeba224 <80118706+amoeba224@users.noreply.github.com>
2022-12-17 00:12:58 +0900

    Add files via upload

--- front/src/components/TreeComponent.jsx
+++ front/src/components/TreeComponent.jsx
@@ -1,74 +1,37 @@
-import { React, useEffect, useState } from "react";
+import React from 'react';
 import styled from 'styled-components';
+import { useRecoilValue } from 'recoil';
+import { temporaryTreeAtom } from '../Atom';
 import Leaf from './Leaf';
 
-// 화면에 tree가 보인다.
-// 작성된 롤링페이퍼 수만큼 요소가 보인다.
-// 각각의 요소는 클릭 가능하다.
-const ATree = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-contents: center;
-    align-items: center;
-    width: 25%;
-    height: 90%;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate( -50%, -50% );
-`;
+const startX = 130;
+const startY = 280;
+const WIDTH = 250;
+const HEIGHT = 450;
 
-const Leaves = styled.div`
-    display: grid;
-    grid-template-columns: repeat(4,1fr);
-    grid-template-rows: repeat(4,1fr);
-    width: 100%;
-    height : 80%;
-`;
-
-const Pillar = styled.div`
-    width: 50%;
-    height: 20%;
-    background-color: brown;
-`;
-
-function Tree() {
-    const [loading, setLoading] = useState(true);
-    const [leaves, setLeaves] = useState([]);
-    const getTree = async() => {
-        const json = await(await fetch(
-            `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-        )).json();
-        setLeaves(json.data.movies);
-        setLoading(false);
-    };
-    useEffect(()=> {
-        getTree();
-    },[]);
-
-    return(
-        <div>
-            {loading ? (
-                <h1>나무를 불러오고 있어요</h1>
-            ) : (
-                <div>
-                    <ATree>
-                        <Leaves>
-                            {leaves.map((leaf)=> (
-                                <Leaf
-                                    key={leaf.id}
-                                    id={leaf.id}
-                                    CoverImg={leaf.medium_cover_image}
-                                />
-                            ))}
-                        </Leaves>
-                        <Pillar/>
-                    </ATree>
-                </div>
-            )}
-        </div>
-    );
+function randomX(){
+    return (Math.random()*WIDTH)+startX
 }
 
+function randomY(){
+    return (Math.random()*HEIGHT)+startY
+}
 
-export default Tree;
+const Img = styled.img`
+    position: absolute;
+    left: ${randomX}px;
+    top: ${randomY}px;
+`
+
+
+function TreeComponent(){
+    const leaves = useRecoilValue(temporaryTreeAtom);
+    // const icons = ['https://github.com/vdnuit/Congraduation/blob/vdnuit/front/src/assets/icons/icon0.png?raw=true','https://github.com/vdnuit/Congraduation/blob/vdnuit/front/src/assets/icons/icon0.png?raw=true','https://github.com/vdnuit/Congraduation/blob/vdnuit/front/src/assets/icons/icon0.png?raw=true'];
+    return (
+        <div>
+            {leaves.map((leaf)=><Img src={leaf}/>)}
+        </div>
+    )
+}
+
+export default TreeComponent;
