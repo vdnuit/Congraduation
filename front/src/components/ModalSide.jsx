5471a492b043479a7df61b8f7acd779b7a41bc69
vdnuit <97578112+vdnuit@users.noreply.github.com>
2023-01-24 19:24:31 +0900

    회원탈퇴 리다이렉트 문제 수정

--- front/src/components/ModalSide.jsx
+++ front/src/components/ModalSide.jsx
@@ -66,6 +66,7 @@ function ModalSide({ setModalOpen }) {
         closeModal(false);
     };
     const onPrompt = () => {
+        navigate(`/`);
         if (cookies.get('provider') === 'kakao') {
             alert(
                 '카카오 로그인 회원탈퇴는 카카오톡>설정>카카오계정>연결된 서비스 관리에서 탈퇴해주세요!'
@@ -78,48 +79,43 @@ function ModalSide({ setModalOpen }) {
             const pinput = prompt('회원 탈퇴를 위해 비밀번호를 입력해주세요.');
 
             console.log(pinput);
-            axios
-                .post("/api/v1/auth/check-password",
-                    {password: pinput}
-                )
-                .then((response) => {
-                    console.log(response.status);
 
-                    if (response.status === 200) {
-                        axios
-                            .delete(`http://localhost:8000/api/v1/users/${isLogin.userId}`)
-                            .then((res) => {
-                                console.log(res.status);
-                                if (res.status === 200) {
-                                    alert('회원탈퇴를 완료했습니다.');
-                                    isLogin.userId = undefined;
-                                    isLogin.nick = undefined;
-                                    navigate(`/`);
-
-                                    closeModal(false);
-                                    return 0;
-                                }
-
-                                alert('회원탈퇴에 실패했습니다.\n다시 시도해주세요.');
+            axios.post('/api/v1/auth/check-password', { password: pinput }).then((response) => {
+                console.log(response.status);
+                if (response.status === 200) {
+                    axios
+                        .delete(`http://localhost:8000/api/v1/users/${isLogin.userId}`)
+                        .then((res) => {
+                            console.log(res.status);
+                            if (res.status === 200) {
+                                alert('회원탈퇴를 완료했습니다.');
                                 isLogin.userId = undefined;
                                 isLogin.nick = undefined;
-                                navigate(`/`);
 
                                 closeModal(false);
                                 return 0;
-                            });
-                    } else {
-                        alert('비밀번호가 일치하지 않습니다.\n다시 시도해주세요!');
-                        isLogin.userId = undefined;
-                        isLogin.nick = undefined;
-                        navigate(`/`);
+                            }
 
-                        closeModal(false);
+                            alert('회원탈퇴에 실패했습니다.\n다시 시도해주세요.');
+                            isLogin.userId = undefined;
+                            isLogin.nick = undefined;
+                            navigate(`/`);
 
-                        return 0;
-                    }
+                            closeModal(false);
+                            return 0;
+                        });
+                } else {
+                    alert('비밀번호가 일치하지 않습니다.\n다시 시도해주세요!');
+                    isLogin.userId = undefined;
+                    isLogin.nick = undefined;
+                    navigate(`/`);
+
+                    closeModal(false);
+
                     return 0;
-                });
+                }
+                return 0;
+            });
         }
         return 0;
     };
