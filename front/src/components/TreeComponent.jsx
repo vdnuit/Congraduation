52d2ef079062cc07d2f4cf62110bc8119362178b
vdnuit <97578112+vdnuit@users.noreply.github.com>
2023-01-25 23:17:41 +0900

    답변 공유 비율 수정

--- front/src/components/TreeComponent.jsx
+++ front/src/components/TreeComponent.jsx
@@ -1,6 +1,6 @@
 /* eslint no-underscore-dangle: 0 */
 
-import { React, useState, useEffect} from 'react';
+import { React, useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { useRecoilValue } from 'recoil';
 import { temporaryTreeAtom } from '../Atom';
@@ -10,49 +10,51 @@ const startY = 60;
 const WIDTH = 50;
 const HEIGHT = 60;
 
-function randomX(){
-    return `${(Math.random()*WIDTH)+startX}%`
+function randomX() {
+    return `${Math.random() * WIDTH + startX}%`;
 }
 
-function randomY(){
-    return `${(Math.random()*HEIGHT)+startY}vw`
+function randomY() {
+    return `${Math.random() * HEIGHT + startY}vw`;
 }
 
-function BigRandomX(){
-    return `${(Math.random()*200)+180}px`
+function BigRandomX() {
+    return `${Math.random() * 200 + 180}px`;
 }
 
-function BigRandomY(){
-    return `${(Math.random()*345)+140}px`
+function BigRandomY() {
+    return `${Math.random() * 345 + 140}px`;
 }
 
 const Img = styled.img`
     position: absolute;
+
+    pointer-events: none;
     width: 44px;
     height: 62px;
-    left: ${props => (props.innerWidth <= 500) ? randomX : BigRandomX};
-    top: ${props => (props.innerWidth <= 500) ? randomY : BigRandomY};
+    left: ${(props) => (props.innerWidth <= 500 ? randomX : BigRandomX)};
+    top: ${(props) => (props.innerWidth <= 500 ? randomY : BigRandomY)};
 `;
 
-
-
-function TreeComponent(){
+function TreeComponent() {
     const leaves = useRecoilValue(temporaryTreeAtom);
     // const icons = ['https://github.com/vdnuit/Congraduation/blob/vdnuit/front/src/assets/icons/icon0.png?raw=true','https://github.com/vdnuit/Congraduation/blob/vdnuit/front/src/assets/icons/icon0.png?raw=true','https://github.com/vdnuit/Congraduation/blob/vdnuit/front/src/assets/icons/icon0.png?raw=true'];
     const [innerWidth, setInnerWidth] = useState(window.innerWidth);
 
     useEffect(() => {
-      const resizeListener = () => {
-        setInnerWidth(window.innerWidth);
-      };
-      window.addEventListener("resize", resizeListener);
+        const resizeListener = () => {
+            setInnerWidth(window.innerWidth);
+        };
+        window.addEventListener('resize', resizeListener);
     });
-    
+
     return (
         <div>
-            {leaves.map((leaf)=><Img src={leaf.paperImage} key={leaf._id} innerWidth={innerWidth}/>)}
+            {leaves.map((leaf) => (
+                <Img src={leaf.paperImage} key={leaf._id} innerWidth={innerWidth} />
+            ))}
         </div>
-    )
+    );
 }
 
 export default TreeComponent;
