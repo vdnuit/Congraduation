a3ba7e0b63b7745f3c4f6f5506eedb95dcc04398
amoeba224 <amoeba224@likelion.org>
2023-02-15 10:16:10 +0900

    separate OS

--- front/src/components/InstaModal.jsx
+++ front/src/components/InstaModal.jsx
@@ -176,7 +176,9 @@ function InstaModal({ setModalOpen }) {
     };
 
     const handleShare = async () => {
-        htmlToImage.toBlob(document.querySelector('.modal')).then((blob) => {
+        const UA = navigator.userAgent.toLowerCase();
+        if(UA.indexOf('android') > -1){
+            htmlToImage.toBlob(document.querySelector('.modal')).then((blob) => {
             const data = {
                 files: [
                     new File([blob], 'letter.png', {
@@ -186,101 +188,101 @@ function InstaModal({ setModalOpen }) {
                 title: 'Letter',
                 text: '소중한 쪽지'
             };
-            toBlob(document.querySelector('.modal')).then((blob1) => {
-                const data1 = {
+
+                if(navigator.canShare && navigator.canShare(data)){
+                    try {
+                        navigator.share(data);
+                    } catch (error) {
+                        alert('이미지를 공유할 수 없습니다.');
+                    }
+                } else {
+                    alert("이미지 공유를 지원하지 않는 브라우저입니다.");
+                }
+            })
+        }
+        if(UA.indexOf('iphone')>-1){
+            htmlToImage.toBlob(document.querySelector('.modal')).then((blob) => {
+                const data = {
                     files: [
-                        new File([blob1], 'letter.png', {
-                            type: blob1.type
+                        new File([blob], 'letter.png', {
+                            type: blob.type
                         })
                     ],
                     title: 'Letter',
                     text: '소중한 쪽지'
                 };
-
-                toBlob(document.querySelector('.modal')).then((blob2) => {
-                    const data2 = {
+                toBlob(document.querySelector('.modal')).then((blob1) => {
+                    const data1 = {
                         files: [
-                            new File([blob2], 'letter.png', {
-                                type: blob2.type
+                            new File([blob1], 'letter.png', {
+                                type: blob1.type
                             })
                         ],
                         title: 'Letter',
                         text: '소중한 쪽지'
                     };
-
-                    toBlob(document.querySelector('.modal')).then((blob3) => {
-                        const data3 = {
+    
+                    toBlob(document.querySelector('.modal')).then((blob2) => {
+                        const data2 = {
                             files: [
-                                new File([blob3], 'letter.png', {
-                                    type: blob3.type
+                                new File([blob2], 'letter.png', {
+                                    type: blob2.type
                                 })
                             ],
                             title: 'Letter',
                             text: '소중한 쪽지'
                         };
-
-                        toBlob(document.querySelector('.modal')).then((blob4) => {
-                            const data4 = {
+    
+                        toBlob(document.querySelector('.modal')).then((blob3) => {
+                            const data3 = {
                                 files: [
-                                    new File([blob4], 'letter.png', {
-                                        type: blob4.type
+                                    new File([blob3], 'letter.png', {
+                                        type: blob3.type
                                     })
                                 ],
                                 title: 'Letter',
                                 text: '소중한 쪽지'
                             };
-
-                            toBlob(document.querySelector('.modal')).then((blob5) => {
-                                const data5 = {
+    
+                            toBlob(document.querySelector('.modal')).then((blob4) => {
+                                const data4 = {
                                     files: [
-                                        new File([blob5], 'letter.png', {
-                                            type: blob5.type
+                                        new File([blob4], 'letter.png', {
+                                            type: blob4.type
                                         })
                                     ],
                                     title: 'Letter',
                                     text: '소중한 쪽지'
                                 };
-
-                                toBlob(document.querySelector('.modal')).then((blob6) => {
-                                    const data6 = {
+    
+                                toBlob(document.querySelector('.modal')).then((blob5) => {
+                                    const data5 = {
                                         files: [
-                                            new File([blob6], 'letter.png', {
-                                                type: blob6.type
+                                            new File([blob5], 'letter.png', {
+                                                type: blob5.type
                                             })
                                         ],
                                         title: 'Letter',
                                         text: '소중한 쪽지'
                                     };
-
-                                    toBlob(document.querySelector('.modal')).then((blob7) => {
-                                        const data7 = {
-                                            files : [
-                                                new File([blob7], "letter.png", {
-                                                    type: blob7.type
-                                                })
-                                            ],
-                                            title: "Letter",
-                                            text: "소중한 쪽지"
-                                        };
-
-                                        if(navigator.canShare && navigator.canShare(data7)){
-                                            try {
-                                                navigator.share(data7);
-                                            } catch (error) {
-                                                alert('이미지를 공유할 수 없습니다.');
-                                            }
-                                        } else {
-                                            alert("이미지 공유를 지원하지 않는 브라우저입니다.");
+    
+                                    if(navigator.canShare && navigator.canShare(data5)){
+                                        try {
+                                             navigator.share(data5);
+                                        } catch (error) {
+                                            alert('이미지를 공유할 수 없습니다.');
                                         }
-                                    })
+                                    } else {
+                                        alert("이미지 공유를 지원하지 않는 브라우저입니다.");
+                                    }
                                 })
                             })
-                        })
-                    })   
+                        })   
+                    })
                 })
             })
-        })
         }
+    }
     
     
 
